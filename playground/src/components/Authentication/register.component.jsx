8dcbe9b665a962a2d65363ce163ac68852783ebe
bmark88 <bradley.mark@hotmail.com>
2020-11-20 12:57:28 -0500

    Create register and sign in components

---
+++ playground/src/components/Authentication/register.component.jsx
@@ -0,0 +1,95 @@
+import React, { useState } from 'react';
+
+const Register = () => {
+  const [register, setRegister] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    password: ''
+  });
+
+  const handleChange = (e) => {
+    setRegister({...register, [e.target.id]: e.target.value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    
+  };
+
+  const { firstName, lastName, email, password } = register;
+  
+  return (
+    <>
+      <h2>Register Form</h2>
+      <form id='registration-form' data-ms-form="signup">
+        <label 
+          htmlFor='fname'
+        >
+          First name:
+        </label>
+        <input 
+          type="text" 
+          id="firstName"
+          name="fname"
+          onChange={handleChange}
+          value={firstName}
+          required
+          data-ms-member="first-name"
+        />
+        <label 
+          htmlFor='lname'
+        >
+          Last name:
+        </label>
+        <input 
+          type="text" 
+          id="lastName"
+          name="lname" 
+          onChange={handleChange}
+          value={lastName}
+          required
+          data-ms-member="last-name"
+        />
+        <label 
+          htmlFor='email'
+        >
+          Email:
+        </label>
+        <input 
+          type="email" 
+          id="email"
+          name="email" 
+          onChange={handleChange}
+          value={email}
+          required
+          data-ms-member="email"
+        />
+        <label 
+          htmlFor='password'
+        >
+          Password:
+        </label>
+        <input 
+          type="password" 
+          id="password"
+          name="password" 
+          onChange={handleChange}
+          value={password}
+          required
+          data-ms-member="password"
+        />
+      </form>
+      <button 
+        data-ms-membership="5fb7fa582250ca0004b336e4"
+        type='submit' 
+        form='registration-form'
+        onSubmit={handleSubmit}
+      >
+        Submit Registration Information
+      </button>
+    </>
+  );
+};
+
+export default Register;
---
+++ playground/src/components/Authentication/sign-in.component.jsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+
+const SignUp = () => {
+  const [signup, setSignUp] = useState({
+    email1: '',
+    password1: ''
+  });
+
+  const handleChange = (e) => {
+    setSignUp({...signup, [e.target.id]: e.target.value})
+  };
+
+  const { email, password } = signup;
+  
+  return (
+    <>
+      <h2>Sign In Form</h2>
+      <form id='sign-in-form' data-ms-form="login">
+        <label htmlFor='email'>
+          Email:
+        </label>
+        <input 
+          type="email" 
+          id="email1"
+          name="email" 
+          onChange={handleChange}
+          value={email}
+          required
+          data-ms-member="email"
+        />
+        <label htmlFor='password'>
+          Password:
+        </label>
+        <input 
+          type="password" 
+          id="password1"
+          name="password" 
+          onChange={handleChange}
+          value={password}
+          required
+          data-ms-member="password"
+        />
+        </form>
+        <button 
+          type='submit' 
+          form='sign-in-form'
+          data-ms-form="login"
+        >
+          Submit Login Information
+        </button>
+        <a href='#/ms/password-reset'>Forgot Password?</a>
+    </>
+  );
+};
+
+export default SignUp;
