e521b4f8ce61e3c7773c2bee400cfa209cf8426a
bmark88 <bradley.mark@hotmail.com>
2020-12-30 17:45:51 -0500

    Add react-router-dom and implement Switch Routes

--- playground/src/App.js
+++ playground/src/App.js
@@ -1,24 +1,31 @@
+import { Route, Switch } from 'react-router';
+
 import './App.css';
 
 // Components
-// import ToDo from './components/to-do.component';
+import ToDo from './components/to-do.component';
 import Register from './components/Authentication/register.component';
 import SignIn from './components/Authentication/sign-in.component';
-// import Docker from './pages/docker';
+import Docker from './pages/docker';
 
 function App() {
-  const user = window.localStorage.getItem('loggedInAs');
+  // const user = window.localStorage.getItem('loggedInAs');
 
-  const clearUserStorage = (e) => {
-    e.preventDefault();
-    localStorage.clear();
-    window.location.reload();
-  };
+  // const clearUserStorage = (e) => {
+  //   e.preventDefault();
+  //   localStorage.clear();
+  //   window.location.reload();
+  // };
 
   return (
     <>
-      {/* <ToDo /> */}
-      {
+      <Switch>
+        <Route path='/docker' render={() => <Docker />}/>
+        <Route exact path='/' render={() => <h1>Welcome To Bradley's React Sandbox</h1>} />
+        {/* <Route path='/to-do-list' render={() => <ToDo />} /> */}
+        <Route path='/login' render={() => <SignIn />}/>
+        <Route path='/register' render={() => <Register />}/>
+      {/* {
         user && 
         <>
           Logged in as: {user}
@@ -31,8 +38,8 @@ function App() {
           <Register />
           <SignIn />
         </>
-      }
-      {/* <Docker /> */}
+      } */}
+        </Switch>
     </>
   );
 }
